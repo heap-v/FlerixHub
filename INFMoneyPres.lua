924143db106c78d008274642dfb5834483cbda7c
s15x <91010121+s15x@users.noreply.github.com>
2022-08-16 20:47:13 +0100

    g.toggle

--- INFMoneyPres.lua
+++ INFMoneyPres.lua
@@ -1,5 +1,5 @@
-_G.Clean = true
-while _G.Clean and wait(.1) do
+_G.Toggled = true
+while _G.Toggled = true wait(.1) do
 game:GetService("ReplicatedStorage").Remotes.RefillRemote:FireServer(true)
 game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(math.huge, math.huge)
 game:GetService("ReplicatedStorage").Remotes.ClientFrameData:FireServer(math.huge , true, true, math.huge)
