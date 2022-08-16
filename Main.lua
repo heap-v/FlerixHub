8a2d742a440f3a03332a335f9c8d49affdfc9c6a
heap <68669985+heap-v@users.noreply.github.com>
2022-08-16 16:51:46 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -8,7 +8,7 @@ local serv = win:Server("Nicos Nextbot", "")
 local btns = serv:Channel("Functions")
 
 btns:Button(
-    "Powerbox ESP + Bot ESP",
+    "Powerbox ESP + Bot ESP - 🟢",
     function()
     loadstring(game:HttpGet("https://fern.wtf/scripts/nico_bot.lua", true))()
         DiscordLib:Notification("Notification", "Done!", "ty.")
@@ -18,7 +18,7 @@ btns:Button(
 btns:Seperator()
 
 btns:Button(
-    "Godmode",
+    "Godmode - 🟢",
     function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/Godmode.lua", true))()
         DiscordLib:Notification("Notification", "Done!", "ty.")
@@ -28,7 +28,7 @@ btns:Button(
 btns:Seperator()
 
 btns:Button(
-    "SEMI-Godmode",
+    "SEMI-Godmode - 🟢",
     function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/Semi-Godmode.lua", true))()
         DiscordLib:Notification("Notification", "Done!", "ty.")
@@ -38,7 +38,7 @@ btns:Button(
 btns:Seperator()
 
 btns:Button(
-    "Open ALL Doors",
+    "Open ALL Doors - 🟢",
     function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/Openalldoors.lua", true))()
         DiscordLib:Notification("Notification", "Done!", "ty.")
@@ -48,7 +48,7 @@ btns:Button(
 btns:Seperator()
 
 btns:Button(
-    "Fullbright",
+    "Fullbright - 🟢",
     function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/FB.lua", true))()
         DiscordLib:Notification("Notification", "Done!", "ty.")
@@ -58,9 +58,73 @@ btns:Button(
 btns:Seperator()
 
 btns:Button(
-    "Auto BHop - Hold Space",
+    "Auto BHop / Hold Space - 🟢",
     function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/AutoBHop.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+local serv = win:Server("Specter", "")
+
+local btns = serv:Channel("Functions")
+
+btns:Button(
+    "Fullbright - 🟠",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/FB.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Ghost ESP - 🟢",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/GESP.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Unlock ALL Gamepasses - 🟠",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/SPECUNAG.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Check Ghost Orbs - 🟢",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/CheckGO.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Never Be Alone - 🟢",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/NeverBA.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Infinite Sanity - 🟢",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/InfSan.lua", true))()
         DiscordLib:Notification("Notification", "Done!", "ty.")
     end
 )
