5fe9340d912c8b77d76b106aec6cf31e4c5c0dcc
heap <68669985+heap-v@users.noreply.github.com>
2022-08-16 16:47:33 +0100

    Create NeverBA.lua

---
+++ NeverBA.lua
@@ -0,0 +1,7 @@
+function NEVER_ALONE_ALWAYS_OUTSIDE_FUNCTION()
+while true do wait(0.1) do 
+game:GetService('Players').LocalPlayer.Data.Alone.Value = false
+end
+end 
+end
+NEVER_ALONE_ALWAYS_OUTSIDE_FUNCTION()
