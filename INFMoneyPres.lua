322aa022d6d965d046f34965c431c9642d490359
s15x <91010121+s15x@users.noreply.github.com>
2022-08-17 11:55:53 +0100

    Update INFMoneyPres.lua

--- INFMoneyPres.lua
+++ INFMoneyPres.lua
@@ -1,6 +1,4 @@
-_G.Clean = true
-while _G.Clean wait(.1) do
+wait(.1)
 game:GetService("ReplicatedStorage").Remotes.RefillRemote:FireServer(true)
 game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(math.huge, math.huge)
 game:GetService("ReplicatedStorage").Remotes.ClientFrameData:FireServer(math.huge , true, true, math.huge)
-end
