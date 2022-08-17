2d7f34aa4f56f332452a1fd510f093cddcb74531
s15x <91010121+s15x@users.noreply.github.com>
2022-08-17 12:22:11 +0100

    Update INFMoneyPres.lua

--- INFMoneyPres.lua
+++ INFMoneyPres.lua
@@ -1,4 +1,7 @@
+getgenv().autosell = v
+while true do
 wait(.1)
 game:GetService("ReplicatedStorage").Remotes.RefillRemote:FireServer(true)
 game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(math.huge, math.huge)
 game:GetService("ReplicatedStorage").Remotes.ClientFrameData:FireServer(math.huge , true, true, math.huge)
+end
