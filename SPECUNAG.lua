6176de1ab25049079aae95a5352f4443e15a1c73
heap <68669985+heap-v@users.noreply.github.com>
2023-01-27 00:55:28 +0000

    Delete SPECUNAG.lua

--- SPECUNAG.lua
+++
@@ -1,7 +0,0 @@
-local function UNLOCK_GAMEPASSES() --Its only for Characters, i didnt test for other things
-for i,v in pairs(game:GetService('Players').LocalPlayer.Gamepasses:GetChildren()) do
-v.Value = true
-end
-end
-
-UNLOCK_GAMEPASSES()
