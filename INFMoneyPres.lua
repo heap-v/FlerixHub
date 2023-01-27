66cf4aba84c156b6638c0c4b7ecdeb5efd6df6c6
heap <68669985+heap-v@users.noreply.github.com>
2023-01-27 00:49:40 +0000

    Delete INFMoneyPres.lua

--- INFMoneyPres.lua
+++
@@ -1,7 +0,0 @@
-getgenv().autosell = v
-while getgenv().autosell == true do
-wait(.1)
-game:GetService("ReplicatedStorage").Remotes.RefillRemote:FireServer(true)
-game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(math.huge, math.huge)
-game:GetService("ReplicatedStorage").Remotes.ClientFrameData:FireServer(math.huge , true, true, math.huge)
-end
