159aad3a5e186ceaae4682df2d6f827ec51c432c
heap <68669985+heap-v@users.noreply.github.com>
2023-01-27 00:54:04 +0000

    Delete AutoBHop.lua

--- AutoBHop.lua
+++
@@ -1,8 +0,0 @@
-local LocalPlayer = game.Players.LocalPlayer
-
-game:GetService("RunService").Heartbeat:Connect(function()
-   if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid") then
-       LocalPlayer.Character.Humanoid.Jump = true
-       LocalPlayer.Character.Humanoid.JumpPower = 40
-   end
-end)
