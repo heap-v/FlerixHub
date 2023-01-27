862298a4a43392b3a42b208c6de6c51c9b72ac9a
heap <68669985+heap-v@users.noreply.github.com>
2023-01-27 00:55:37 +0000

    Delete NeverBA.lua

--- NeverBA.lua
+++
@@ -1,7 +0,0 @@
-function NEVER_ALONE_ALWAYS_OUTSIDE_FUNCTION()
-while true do wait(0.1) do 
-game:GetService('Players').LocalPlayer.Data.Alone.Value = false
-end
-end 
-end
-NEVER_ALONE_ALWAYS_OUTSIDE_FUNCTION()
